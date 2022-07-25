dd0a95cf5784eb895ca897f3e0e61840440d03f3
Orion Browne <orion.browne@gmail.com>
2022-07-25 12:51:07 -0400

    Updated to pull just steps from reponse

--- APIreq.py
+++ APIreq.py
@@ -2,12 +2,13 @@ import requests
 import json
 
 
+##-----------------------------------------------Spoonacular API Call---------------------------------------------------
 ##IMPORTANT FOR LOCAL RUN
 ##Get your rapid api key and host from https://rapidapi.com/spoonacular/api/recipe-food-nutrition/
 ##After logging in with GitHub click the subscribe to test and the free option
 ##The header parameters are in any of the sample requests, paste key in the quotes
 headers = {
-	"X-RapidAPI-Key": "PASTE_KEY_HERE",
+	"X-RapidAPI-Key": "0369576e5bmsh492d05bb416cdb5p10406fjsn243cabb13c2e",
 	"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
 }
 
@@ -38,7 +39,18 @@ data = response.json()
 ##Saves steps in the form steps: {'number': , 'step':...}
 steps = data[0]["steps"]
 print(steps)
+one = steps[0]["step"]
+textquery = ""
+for i in steps:
+    textquery = textquery + i["step"] + "\n"
+##-----------------------------------------------Meaningcloud API Call---------------------------------------------------
+payload={
+    'key': '8b38fc74a8a6e9be5b5eef2c7a768fd9',
+    'lang': 'ENGLISH',
+    'txt': 'YOUR TEXT HERE'
+}
 
+##response = requests.post(url, data=payload)
 
 
 

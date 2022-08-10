b1d8ef6671e572430885ac3294e7a7256f6465bd
Orion Browne <orion.browne@gmail.com>
2022-08-09 21:45:15 -0400

    Made python into an executable, added steps.txt for output

--- APIreq.py
+++ APIreq.py
@@ -15,23 +15,29 @@ headers = {
 
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch"
 ##query for the GET Search Recipes request
-querystring = {"query":"pasta",
-               "includeIngredients":"tomato",
+search = input("Enter a food or recipe")
+ingredients = input("Enter ingredients seperated by ','")
+idNum = int(input("Enter number of recipes to output."))
+while(idNum >= 10):
+    idNum = input("Enter number of recipes to output.")
+querystring = {"query":search,
+               "includeIngredients":ingredients,
                "sort":"popularity"
                }
 ##                         ^ modify this parameter
 
 ##Inital API GET request to Recipe Search for list of  recipes
-response = requests.request("GET", url, headers=headers, params=querystring)
+searchResults = requests.request("GET", url, headers=headers, params=querystring)
+
 
 
 ##Convert json to a dict so it's parseable
-data = response.json()
+data = searchResults.json()
 
 ##Gets ids of recipes and stores them in idlist
 idlist = []
 allSteps = ""
-for i in range(3):
+for i in range(idNum):
     ids = data['results'][i]['id']
     idlist.append(str(ids))
 
@@ -41,12 +47,14 @@ for i in range(len(idlist)):
     querystring = {"stepBreakdown": "true"}
     response = requests.request("GET", url, headers=headers, params=querystring)
     datastep = response.json()
-    steps = datastep[0]["steps"]
-    for j in steps:
-        allSteps = allSteps + j["step"] + "\n"
-
+    if len(datastep):
+        steps = datastep[0]["steps"]
+        for j in steps:
+            allSteps = allSteps + j["step"] + "\n"
 print(allSteps)
-
+file = open("steps.txt", "w")
+file.writelines(allSteps)
+file.close()
 
 
 

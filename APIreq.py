5e438915b01ac5dff7666cc762f99438add12583
Orion Browne <orion.browne@gmail.com>
2022-08-11 15:26:58 -0400

    Small changes, comments

--- APIreq.py
+++ APIreq.py
@@ -1,47 +1,43 @@
 import requests
-import json
-
 
 ##-----------------------------------------------Spoonacular API Call---------------------------------------------------
-##IMPORTANT FOR LOCAL RUN
-##Get your rapid api key and host from https://rapidapi.com/spoonacular/api/recipe-food-nutrition/
-##After logging in with GitHub click the subscribe to test and the free option
-##The header parameters are in any of the sample requests, paste key in the quotes
+## IMPORTANT FOR LOCAL RUN
+## Get your rapid api key and host from https://rapidapi.com/spoonacular/api/recipe-food-nutrition/
+## After logging in with GitHub click the subscribe to test and the free option
+## The header parameters are in any of the sample requests, paste key in the quotes
 headers = {
 	"X-RapidAPI-Key": "0369576e5bmsh492d05bb416cdb5p10406fjsn243cabb13c2e",
 	"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
 }
-
 
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch"
 ##query for the GET Search Recipes request
 search = input("Enter a food or recipe")
 ingredients = input("Enter ingredients seperated by ','")
 idNum = int(input("Enter number of recipes to output."))
+
 while(idNum >= 10):
     idNum = input("Enter number of recipes to output.")
+
 querystring = {"query":search,
                "includeIngredients":ingredients,
                "sort":"popularity"
                }
-##                         ^ modify this parameter
 
-##Inital API GET request to Recipe Search for list of  recipes
+## Inital API GET request to Recipe Search for list of  recipes
 searchResults = requests.request("GET", url, headers=headers, params=querystring)
 
-
-
-##Convert json to a dict so it's parseable
+## Convert json to a dict so it's parseable
 data = searchResults.json()
 
-##Gets ids of recipes and stores them in idlist
+## Gets ids of recipes and stores them in idlist
 idlist = []
 allSteps = ""
 for i in range(idNum):
     ids = data['results'][i]['id']
     idlist.append(str(ids))
 
-##For each id: call analyzedInstructions endpoint and store just the steps seperated by "/n" in allSteps
+## For each id: call analyzedInstructions endpoint and store just the steps seperated by "/n" in allSteps
 for i in range(len(idlist)):
     url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + idlist[i] + "/analyzedInstructions"
     querystring = {"stepBreakdown": "true"}
@@ -51,6 +47,8 @@ for i in range(len(idlist)):
         steps = datastep[0]["steps"]
         for j in steps:
             allSteps = allSteps + j["step"] + "\n"
+
+## Output steps into txt file and console
 print(allSteps)
 file = open("steps.txt", "w")
 file.writelines(allSteps)
